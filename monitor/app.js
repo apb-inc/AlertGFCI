ce283ed26bbd1c2ed1ed1a76208c8509e0efac43
Connor Knabe <connorknabe@gmail.com>
2016-01-08 11:59:03 -0600

    Adding modules

---
+++ monitor/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+// Route settings
+app.use('/', router);
+app.listen(port);
+console.log('Magic happens on port ' + port +" - "+ new Date());
+
+
+
+router.get('/', function(req,res){
+    res.send({"status":"200"});        
+});
