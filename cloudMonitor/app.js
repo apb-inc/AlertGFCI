d27e7828dc29e9cd56b13c3f1aa6eed2732ee34e
ObiwanBox <connorknabe@gmail.com>
2016-01-27 11:37:40 -0500

    Using correct date for emails

--- cloudMonitor/app.js
+++ cloudMonitor/app.js
@@ -41,8 +41,10 @@ setInterval(function(){
 
 
 function sendEmail(type){
+
     var currentTime = new Date();
-    var emailContent;
+	currentTime = new Date(currentTime.getTime()-60*60*1000);
+	var emailContent;
 
     if(type == "online"){
         emailContent = "The Holka server is back online "+currentTime;
