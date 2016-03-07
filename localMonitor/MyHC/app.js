c0c719bde95dc666bb173899feda4a75709342a5
ObiwanBox <connorknabe@gmail.com>
2016-03-07 09:08:50 -0500

    Updating gitignore

--- localMonitor/MyHC/app.js
+++ localMonitor/MyHC/app.js
@@ -1,6 +1,8 @@
+var request = require('request');
+var nodemailer = require('nodemailer');
 var express = require('express');
 var app = express();
-var request = require('request');
+
 var port = process.env.PORT || 8188;
 var router = express.Router();
 // Route settings
@@ -19,24 +21,67 @@ setInterval(function(){
     }
 }, 2000);
 
-
 function serviceObjectFromName(serviceName){
     var found = services.filter(function(item) { return item.name === serviceName; });
     return found[0];
 }
 
-function sendEmail(content){
+function sendMessage(content){
+
     //Check if Twilio is online if offline use Node mailer
+
+	request.post({
+          url:	serverInfo.twilioSendServer,
+          form: { toNumber: toNumber, fromNumber:loginInfo.holkaAlertNumber, message: msgContent, twilioLocalKey: loginInfo.twilioLocalKey }
+    }, function(error, response, body){
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        } else {
+            sendEmail();
+        }
+    });
+
+
     console.log("sending email "+content);
+}
+
+
+function sendEmail(email,type){
+    var emailContent = "";
+    var currentTime = new Date();
+
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: loginInfo.gmailHolkaUser,
+            pass: loginInfo.gmailHolkaPass
+        }
+    });
+    var mailOptions = {
+        from: loginInfo.fromEmail,
+        to: email,
+        subject: 'Floatee update!',
+        text: emailContent
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
+
+function sendEmail(content){
+
 }
 
 function sendAlert(serviceObj, isOnline){
     var serviceName = serviceObj.name;
         if(isOnline){
-            sendEmail(new Date()+serviceName+" is online!");
+            sendMessage(new Date()+serviceName+" is online!");
             serviceObj.needsToSend = true;
         } else if(serviceObj.needsToSend) {
-            sendEmail(new Date()+serviceName+" is offline!");
+            sendMessage(new Date()+serviceName+" is offline!");
             serviceObj.needsToSend = false;
         }
 
