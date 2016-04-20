26a9c2dd342c94cddabb8f4b56211d90b719544b
Connor Knabe <connorknabe@gmail.com>
2016-04-20 07:24:36 -0500

    Adding routes

--- peroxidePump/app.js
+++ peroxidePump/app.js
@@ -3,11 +3,27 @@ var app = express();
 var port = process.env.PORT || 2610;
 var router = express.Router();
 var request = require('request');
+var Gpio = require('onoff').Gpio;
+var pump = new Gpio(22, 'out');
+
+
 app.listen(port);
 console.log('Magic happens on port ' + port +" - "+ new Date());
 
 
 
 app.get('/', function(req,res){
+    res.send({"status":"200"});
+
+});
+
+
+app.get('/on', function(req,res){
+    pump.writeSync(1);
+    res.send({"status":"200"});
+});
+
+app.get('/off', function(req,res){
+    pump.writeSync(0);
     res.send({"status":"200"});
 });
